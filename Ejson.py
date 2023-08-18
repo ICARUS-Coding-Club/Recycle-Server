01aa1cc4fb959a599fd2ffe230389d473d31933c
ddungong <kmg000719@naver.com>
2023-08-18 20:20:27 +0900

    액셀을 json 바꿔주는 코드

--- Ejson.py
+++ Ejson.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 
 def excel_to_json(input_filename, json_output_filename):
-    # Read the Excel file into a DataFrame
+    # Excel 파일을 DataFrame 형태로 읽기
     df = pd.read_excel(input_filename)
 
-    # Convert the DataFrame to JSON format (string)
+    # DataFrame을 JSON 형식의 문자열로 변환하기
     json_str = df.to_json(orient='split', index=False, force_ascii=False)
 
-    # Write the JSON string to a file with UTF-8 encoding
+    # UTF-8 인코딩으로 JSON 문자열을 파일에 쓰기
     with open(json_output_filename, 'w', encoding='utf-8') as json_file:
         json_file.write(json_str)
 
 
-# Example usage:
+# 예제 사용 방법:
 excel_to_json(r'C:\Users\kmg00\Desktop\생활쓰레기종합배출정보.xlsx', r'C:\Users\kmg00\Desktop\Rdata\output.json')
