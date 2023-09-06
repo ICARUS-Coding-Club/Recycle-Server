a8294208006c7f1efd7952cd4ebe5dbedd87a59c
ddungong <113035619+ddungong@users.noreply.github.com>
2023-09-06 16:54:01 +0900

    Delete web_moule directory

--- web_moule/Trash.py
+++
@@ -1,8 +0,0 @@
-class News:
-    def __init__(self, title, body, time, acticle, image):
-        self.name = title
-        self.tags = body
-        self.recyclable = time
-        self.categories = acticle
-        self.disposal_method = image
-
